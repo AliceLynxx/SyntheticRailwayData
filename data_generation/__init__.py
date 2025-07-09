d3b872767f42728be2c913e63a9176837bf3588e
AliceLynxx <167111971+AliceLynxx@users.noreply.github.com>
2025-07-09 08:49:27 +0200

    Maak data_generation package aan

---
+++ data_generation/__init__.py
@@ -0,0 +1,9 @@
+"""
+Data generation package voor synthetische spoorwegonderhoudsdata.
+
+Dit package bevat modules voor het laden van stationsdata, genereren van
+onderhoudsrecords en exporteren van data naar verschillende formaten.
+"""
+
+__version__ = "1.0.0"
+__author__ = "SyntheticRailwayData Project"
